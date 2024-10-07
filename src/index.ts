080a233a291707ac78c079f22e95dc1ead4592d6
kishandsyncortex <kishandholakiya1027@gmail.com>
2024-10-07 16:01:52 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -56,3 +56,35 @@ io.on('connection', (socket: Socket) => {
 // DB_USER = postgres.xlkbjgmaxgvhhllutjzo
 // DB_HOST = aws-0-ap-south-1.pooler.supabase.com
 // ACCESS_TOKEN_SECRET = gvjft84gifgnierwr83rsnvsijerhwe9ureth34fn
+
+// npm init -y
+// npx tsc --init
+// npm install pg cors redis bcrypt nodemon morgan dotenv express ts-node typeorm socket.io typescript nodemailer jsonwebtoken multer express-validator nodemailer-smtp-transport
+// npm install --save-dev husky eslint prettier globals @eslint/js @types/bcrypt @types/morgan lint-staged @types/multer typescript-eslint @types/nodemailer @types/jsonwebtoken eslint-config-prettier eslint-plugin-prettier @types/nodemailer-smtp-transport
+
+// package.json
+// "scripts": {
+//     "build": "npx tsc",
+//     "lint": "eslint .",
+//     "prepare": "husky install",
+//     "lint:fix": "eslint . --fix",
+//     "format": "prettier --write .",
+//     "start": "nodemon src/index.ts",
+//     "test": "echo \"Error: no test specified\" && exit 1"
+//   },
+
+// mkdir -p upload && \
+// mkdir -p src/{routes,controllers,entities,middlewares,config,services,utils} && \
+// touch src/index.ts && \
+// touch src/routes/index.ts && \
+// touch src/controllers/{userController.ts,authController.ts} && \
+// touch src/services/{authService.ts,useService.ts} && \
+// touch src/entities/{baseEntity.ts,userEntity.ts} && \
+// touch src/middlewares/{errorMiddleware.ts,validateRequest.ts,validators.ts} && \
+// touch src/config/{dbConfig.ts,appConfig.ts} && \
+// touch src/utils/{errorCodes.ts,resMessages.ts,pagination.ts,errorHandler.ts,token.ts,multer.ts,otp.ts,password.ts,nodemailer.ts,validationMessages.ts,templates.ts} && \
+// echo '{ "semi": true, "singleQuote": true, "trailingComma": "es5", "tabWidth": 2, "printWidth": 300, "arrowParens": "avoid" }' > .prettierrc && \
+// touch .eslintrc.json .gitignore .env && \
+// npm run prepare && \
+// mkdir -p .husky && touch .husky/pre-commit && \
+// chmod +x .husky/pre-commit
