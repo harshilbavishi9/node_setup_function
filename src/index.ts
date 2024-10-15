e7a4f513c023b12ecf4a15def0cf1701440b8ccc
kishandsyncortex <kishandholakiya1027@gmail.com>
2024-10-15 15:50:03 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -44,50 +44,24 @@ io.on('connection', (socket: Socket) => {
   });
 });
 
-// PORT = 8080
-// DB_PORT = 6543
-// SMTP_PORT = 587
-// DB_NAME = postgres
-// DB_TYPE = postgres
-// JWT_EXPIRES_IN = 1h
-// DB_PASS = man_patel_555
-// SMTP_HOST = smtp.gmail.com
-// SMTP_PASS = gtdscvclmzpttujr
-// SMTP_USER = nitiril208@gmail.com
-// BASE_URL = http://localhost:8080
-// REDIS_URL = redis://127.0.0.1:6379
-// DB_USER = postgres.xlkbjgmaxgvhhllutjzo
-// DB_HOST = aws-0-ap-south-1.pooler.supabase.com
-// ACCESS_TOKEN_SECRET = gvjft84gifgnierwr83rsnvsijerhwe9ureth34fn
-
-// npm init -y
-// npx tsc --init
-// npm install pg cors redis bcrypt nodemon morgan dotenv express ts-node typeorm socket.io typescript nodemailer jsonwebtoken multer express-validator nodemailer-smtp-transport
-// npm install --save-dev husky eslint prettier globals @eslint/js @types/bcrypt @types/morgan lint-staged @types/multer typescript-eslint @types/nodemailer @types/jsonwebtoken eslint-config-prettier eslint-plugin-prettier @types/nodemailer-smtp-transport
-
-// package.json
-// "scripts": {
-//     "build": "npx tsc",
-//     "lint": "eslint .",
-//     "prepare": "husky install",
-//     "lint:fix": "eslint . --fix",
-//     "format": "prettier --write .",
-//     "start": "nodemon src/index.ts",
-//     "test": "echo \"Error: no test specified\" && exit 1"
+// {
+//   "baseUrl": "http://localhost:8080",
+//   "redisUrl": "redis://127.0.0.1:6379",
+//   "jwtExpiresIn": "1h",
+//   "accessTokenSecret": "gvjft84gifgnierwr83rsnvsijerhwe9ureth34fn",
+//   "port": 8080,
+//   "smtp": {
+//     "smtpHost": "smtp.gmail.com",
+//     "smtpPort": 587,
+//     "smtpUser": "nitiril208@gmail.com",
+//     "smtpPass": "gtdscvclmzpttujr"
 //   },
-
-// mkdir -p upload && \
-// mkdir -p src/{routes,controllers,entities,middlewares,config,services,utils} && \
-// touch src/index.ts && \
-// touch src/routes/index.ts && \
-// touch src/controllers/{userController.ts,authController.ts} && \
-// touch src/services/{authService.ts,useService.ts} && \
-// touch src/entities/{baseEntity.ts,userEntity.ts} && \
-// touch src/middlewares/{errorMiddleware.ts,validateRequest.ts,validators.ts} && \
-// touch src/config/{dbConfig.ts} && \
-// touch src/utils/{errorCodes.ts,resMessages.ts,pagination.ts,errorHandler.ts,token.ts,multer.ts,otp.ts,password.ts,nodemailer.ts,validationMessages.ts,templates.ts} && \
-// echo '{ "semi": true, "singleQuote": true, "trailingComma": "es5", "tabWidth": 2, "printWidth": 300, "arrowParens": "avoid" }' > .prettierrc && \
-// touch .eslintrc.json .gitignore && \
-// npm run prepare && \
-// mkdir -p .husky && touch .husky/pre-commit && \
-// chmod +x .husky/pre-commit
+//   "db": {
+//     "dbHost": "aws-0-ap-south-1.pooler.supabase.com",
+//     "dbPort": 6543,
+//     "dbName": "postgres",
+//     "dbUser": "postgres.xlkbjgmaxgvhhllutjzo",
+//     "dbPass": "man_patel_555",
+//     "dbType": "postgres"
+//   }
+// }
